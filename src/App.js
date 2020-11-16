a2153cccc5a7bc1645ccbd5c2fbefa0a1639db72
Claudio <claudio.rtega2701@gmail.com>
2020-11-16 12:05:14 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import styles from './App.module.css';
+// import logo from './logo.svg';
 import './App.css';
+import Map from './Map.js';
 
 function App() {
+
+  const [viewState, setViewState] = React.useState({
+    longitude: -77,
+    latitude: -12.05,
+    zoom: 11,
+    pitch: 50,
+    bearing: -1.45,
+  });
+  const handleChangeViewState = ({ viewState }) => setViewState(viewState);
+
+  const [brushEnabled, setBrush] = React.useState(true);
+  const handleToggleBrush = () => setBrush(!brushEnabled)
+
+  const [blocksPressureEnabled, setBlocksPressure] = React.useState(true);
+  const handleToggleBlocksPressure = () => setBlocksPressure(!blocksPressureEnabled)
+
+  const [hexsPressureEnabled, setHexsPressure] = React.useState(false);
+  const handleToggleHexsPressure = () => setHexsPressure(!hexsPressureEnabled)
+
+  const [blocksAccesibilityEnabled, setBlocksAccesibility] = React.useState(false);
+  const handleToggleBlocksAccesibility = () => setBlocksAccesibility(!blocksAccesibilityEnabled)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Map
+      width="100vw"
+      height="100vh"
+      viewState={viewState}
+      onViewStateChange={handleChangeViewState}
+      blocksPressureEnabled={blocksPressureEnabled}
+      hexsPressureEnabled={hexsPressureEnabled}
+      blocksAccesibilityEnabled={blocksAccesibilityEnabled}
+      brushEnabled={brushEnabled}
+    />
+    <div className={styles.controls}>
+      <button onClick={handleToggleHexsPressure} style={{backgroundColor: hexsPressureEnabled ? "#28a745" : "#333"}}>Pressure Indicator Agg. by Hexs</button>
+      <button onClick={handleToggleBlocksPressure}  style={{backgroundColor: blocksPressureEnabled ? "#28a745" : "#333"}}>Pressure Indicator Agg. by Blocks</button>
+      <button onClick={handleToggleBlocksAccesibility}  style={{backgroundColor: blocksAccesibilityEnabled ? "#28a745" : "#333"}}>Accesibility Indicator Agg. by Blocks</button>
+    </div>
     </div>
   );
 }
---
+++ src/Map.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import StaticMap from 'react-map-gl';
+import DeckGL from '@deck.gl/react';
+import { PolygonLayer, GeoJsonLayer } from '@deck.gl/layers';
+import { BrushingExtension } from '@deck.gl/extensions';
+import { scaleSequential, interpolateViridis, rgb } from 'd3';
+
+
+export default function Map({
+  width,
+  height,
+  viewState,
+  onViewStateChange,
+  blocksPressureEnabled,
+  hexsPressureEnabled,
+  blocksAccesibilityEnabled,
+  brushEnabled,
+
+}) {
+
+  let blocksPressureCm = scaleSequential(interpolateViridis)
+  .domain([0, 1.5]);
+
+  let hexsPressureCm = scaleSequential(interpolateViridis)
+  .domain([-12, 84]);
+
+  let blocksAccesibilityCm = scaleSequential(interpolateViridis)
+  .domain([0, 0.0119]);
+
+  const [hoverInfo, setHoverInfo] = React.useState({});
+
+  const layers = [
+      new GeoJsonLayer({
+        id: 'blocks-pressure',
+        data: '/data/blocks_pressure.geojson',
+        opacity: 0.5,
+        getFillColor: f => Object.values(rgb(blocksPressureCm(f.properties.ds))).splice(0,3),
+        stroked: false,
+        pickable: true,
+        onHover: info => setHoverInfo(info),
+        visible: blocksPressureEnabled,
+      }),
+      new GeoJsonLayer({
+        id: 'hexs-pressure',
+        data: '/data/hexs_pressure.geojson',
+        opacity: 0.5,
+        getFillColor: f => Object.values(rgb(hexsPressureCm(f.properties.ds))).splice(0,3),
+        stroked: false,
+        pickable: true,
+        onHover: info => setHoverInfo(info),
+        visible: hexsPressureEnabled,
+      }),
+      new GeoJsonLayer({
+        id: 'blocks-accesibility',
+        data: '/data/hu_accesibility_blocks_1.geojson',
+        opacity: 0.5,
+        getFillColor: f => Object.values(rgb(blocksAccesibilityCm(f.properties.Ai))).splice(0,3),
+        stroked: false,
+        pickable: true,
+        onHover: info => setHoverInfo(info),
+        visible: blocksAccesibilityEnabled,
+      }),
+
+  ];
+
+  const getTooltip = info => {
+    let str = "";
+    if (!info.object) {
+      return null;
+    } else if (info.object.properties.ds) {
+      str += `ds: ${info.object.properties.ds.toFixed(2)}`
+    } else if (info.object.properties.Ai) {
+      str += `Ai: ${info.object.properties.Ai.toFixed(2)}`
+    }
+    return str
+  }
+
+  return (
+    <DeckGL
+      initialViewState={viewState}
+      controller={true}
+      layers={layers}
+      getTooltip={getTooltip}
+    >
+      <StaticMap
+        reuseMaps
+        mapStyle={"https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"}
+        preventStyleDiffing
+      />
+    </DeckGL>
+  )
+}
